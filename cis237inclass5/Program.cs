da707ab8d9262d147be0b8f2f028eb2177b23949
John Harvey <jharvey7136@mail.kvcc.edu>
2015-11-10 15:44:51 -0500

    First commit

--- cis237inclass5/Program.cs
+++ cis237inclass5/Program.cs
@@ -10,6 +10,31 @@ namespace cis237inclass5
     {
         static void Main(string[] args)
         {
+            AdventureWorks2012Entities adventure = new AdventureWorks2012Entities();
+
+            int counter = 0;
+            Guid id = new Guid();
+
+            foreach (Person person in adventure.People)
+            {
+                if (counter == 0)
+                {
+                    id = person.rowguid;
+                }
+                if (counter++ > 20)
+                    break;
+                Console.WriteLine(person.FirstName + " " + person.LastName + " " + person.rowguid);
+            }
+            Console.WriteLine();
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine(adventure.People.Find(id));
+
+            /*
+            foreach (EmailAddress email in adventure.EmailAddresses)
+            {
+                Console.WriteLine(email);
+            }
+             */
         }
     }
 }
